b0e5d8ef3752e336fe60a17db52e69172ebba92d
LeDrew2017 <33235594+LeDrew2017@users.noreply.github.com>
2023-06-09 20:05:21 -0400

    d1: bootleg bring-up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The bootlegOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_d1.mk
+    $(LOCAL_DIR)/bootleg_d1.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_d1-eng \
-    lineage_d1-user \
-    lineage_d1-userdebug
+    bootleg_d1-eng \
+    bootleg_d1-user \
+    bootleg_d1-userdebug
